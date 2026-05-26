2c1fcbe74a392b203646cc0e27aee7d4bae82b99
mldlbs <903288675@qq.com>
2026-05-26 23:34:35 +0800

    chore: scaffold electron-vite project

---
+++ src/main/index.ts
@@ -0,0 +1,38 @@
+import { app, BrowserWindow, ipcMain } from 'electron'
+import { join } from 'path'
+
+let mainWindow: BrowserWindow | null = null
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 400,
+    height: 500,
+    transparent: true,
+    frame: false,
+    resizable: false,
+    alwaysOnTop: true,
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  })
+
+  if (process.env.ELECTRON_RENDERER_URL) {
+    mainWindow.loadURL(process.env.ELECTRON_RENDERER_URL)
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>秋山澪</div>
+}
+
+export default App
